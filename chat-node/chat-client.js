9f23b357933d297de16c7bc4dee799b51b37fdf6
ffrezende <fabriciofraga15@gmail.com>
2019-08-23 14:49:59 -0300

    web-socket chat

---
+++ chat-node/chat-client.js
@@ -0,0 +1,25 @@
+const socket = require('socket.io-client')('http://localhost:3000');
+const repl = require('repl');
+const chalk = require('chalk');
+let username = '';
+
+socket.on('disconnect', () => {
+  socket.emit('disconnect');
+});
+
+socket.on('connect', () => {
+  console.log(chalk.red('=== start chatting ==='));
+  username = process.argv[2];
+});
+
+socket.on('message', data => {
+  const { cmd, username } = data;
+  console.log(chalk.green(username + ': ' + cmd.split('\n')[0]));
+});
+
+repl.start({
+  prompt: '',
+  eval: cmd => {
+    socket.send({ cmd, username });
+  }
+});
